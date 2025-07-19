9fc250ffd04338f4ae66c461fb7902eb0ca51f9e
Eric Mbithi <mbithieri@gmail.com>
2025-07-19 22:31:19 +0300

    Use props to make a dynamic component template Pass props from a parent component to a child component

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,15 @@
 import React from "react";
-import NavBar from "./NavBar";
-import Home from "./Home";
-import About from "./About";
-
-// pass this data down as props to the child component(s) that need it!
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import About from "./components/About";
 import user from "../data/user";
-console.log(user);
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={user.name} city={user.city} />
+      <About bio={user.bio} links={user.links} />
     </div>
   );
 }
